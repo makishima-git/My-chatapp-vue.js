dfcc94a3c41575fe039f17050081fb8e44488e66
Makiweb <makiweb.max@gmail.com>
2021-11-10 17:26:32 +0900

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+/* import Home from "../views/Home.vue"; */
+import UserList from "../views/UserList.vue";
+import ChatBoard from "../views/ChatBoard.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "UserList",
+    component: UserList,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/chat",
+    name: "ChatBoard",
+    component: ChatBoard,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
